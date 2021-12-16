f19102f30e93022736191bd9ba3d6761059e8956
chlgpgus57 <chlgpgus57@naver.com>
2021-12-16 22:41:12 +0900

    12/16

---
+++ 백준6198옥상정원.java
@@ -0,0 +1,53 @@
+package hh.coding.practice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+	public class Baekjoon6198옥상정원 {
+	
+		static int n, temp;
+		static long result;
+		static int[] arr;
+		static Stack<Integer> s = new Stack<>();
+
+		public static void main(String[] args) throws Exception {
+			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			n = Integer.parseInt(bf.readLine());
+			arr = new int[n];
+
+			for (int i = 0; i < n; i++)
+				arr[i] = Integer.parseInt(bf.readLine());
+
+			for (int i = 0; i < n; i++) {
+				if (s.isEmpty())
+					s.push(i);
+				else {
+					if (arr[s.peek()] > arr[i]) { // 새로 넣을 숫자가 더 작으면
+						s.push(i);
+					} else { // 새로 넣을 숫자가 더 크면, 새로 넣을 숫자가 더 작을때 까지 pop한다.
+						while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+							temp = s.pop();
+							result += (i - temp - 1);
+						}
+						s.push(i);
+					}
+				}
+			}
+			int temp = 0;
+			if (!s.isEmpty()) {
+				temp = s.peek();
+			}
+			while (!s.isEmpty()) {
+				result += (temp - s.pop());
+			}
+
+			System.out.println(result);
+		}
+
+	}
+
